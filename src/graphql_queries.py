34884bf92a43de154835152797b0878462490508
abdul-rafay-mirza <246260926+abdul-rafay-mirza@users.noreply.github.com>
2026-07-10 18:27:38 +0500

    Add Viewer Query and USer Query from anilist

--- src/graphql_queries.py
+++ src/graphql_queries.py
@@ -3,9 +3,233 @@ query {
   Viewer {
     id
     name
-    avatar { large }
+    about
+
+    avatar {
+      large
+    }
+
+    bannerImage
+    unreadNotificationCount
+    donatorTier
+    donatorBadge
+    moderatorRoles
+
+    options {
+      titleLanguage
+      staffNameLanguage
+      restrictMessagesToFollowing
+      airingNotifications
+      displayAdultContent
+      profileColor
+
+      notificationOptions {
+        type
+        enabled
+      }
+
+      disabledListActivity {
+        type
+        disabled
+      }
+    }
+
     mediaListOptions {
       scoreFormat
+      rowOrder
+
+      animeList {
+        customLists
+        sectionOrder
+        splitCompletedSectionByFormat
+        advancedScoring
+        advancedScoringEnabled
+      }
+
+      mangaList {
+        customLists
+        sectionOrder
+        splitCompletedSectionByFormat
+        advancedScoring
+        advancedScoringEnabled
+      }
+    }
+  }
+}
+"""
+
+_USER_QUERY = """
+query ($id: Int, $name: String) {
+  User(id: $id, name: $name) {
+    id
+    name
+
+    previousNames {
+      name
+      updatedAt
+    }
+
+    avatar {
+      large
+    }
+
+    bannerImage
+    about
+    isFollowing
+    isFollower
+    donatorTier
+    donatorBadge
+    createdAt
+    moderatorRoles
+    isBlocked
+    bans
+
+    options {
+      profileColor
+      restrictMessagesToFollowing
+    }
+
+    mediaListOptions {
+      scoreFormat
+    }
+
+    statistics {
+      anime {
+        count
+        meanScore
+        standardDeviation
+        minutesWatched
+        episodesWatched
+
+        genrePreview: genres(limit: 10, sort: COUNT_DESC) {
+          genre
+          count
+        }
+      }
+
+      manga {
+        count
+        meanScore
+        standardDeviation
+        chaptersRead
+        volumesRead
+
+        genrePreview: genres(limit: 10, sort: COUNT_DESC) {
+          genre
+          count
+        }
+      }
+    }
+
+    stats {
+      activityHistory {
+        date
+        amount
+        level
+      }
+    }
+
+    favourites {
+      anime {
+        edges {
+          favouriteOrder
+
+          node {
+            id
+            type
+            status(version: 2)
+            format
+            isAdult
+            bannerImage
+
+            title {
+              userPreferred
+            }
+
+            coverImage {
+              large
+            }
+
+            startDate {
+              year
+            }
+          }
+        }
+      }
+
+      manga {
+        edges {
+          favouriteOrder
+
+          node {
+            id
+            type
+            status(version: 2)
+            format
+            isAdult
+            bannerImage
+
+            title {
+              userPreferred
+            }
+
+            coverImage {
+              large
+            }
+
+            startDate {
+              year
+            }
+          }
+        }
+      }
+
+      characters {
+        edges {
+          favouriteOrder
+
+          node {
+            id
+
+            name {
+              userPreferred
+            }
+
+            image {
+              large
+            }
+          }
+        }
+      }
+
+      staff {
+        edges {
+          favouriteOrder
+
+          node {
+            id
+
+            name {
+              userPreferred
+            }
+
+            image {
+              large
+            }
+          }
+        }
+      }
+
+      studios {
+        edges {
+          favouriteOrder
+
+          node {
+            id
+            name
+          }
+        }
+      }
     }
   }
 }
